23362dbd23580e5aaa511d1eafca845d6b0e0683
sam <samuelhawthorne5@gmail.com>
2021-06-16 10:10:50 -0500

    adding javascript

--- testme/testme.js
+++ testme/testme.js
@@ -1,2 +1,20 @@
-let testMe = alert("Hi");
-let mine = console.log("hey")
+const timer = document.getElementById('timebox');
+let hr = 0;
+let min = 0;
+let sec = 0;
+
+//when to display 0's
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
+//interval
+let interval = null;
+
+// status of stopstart
+
+let status = "stopped";
+
+// stop watch function to determine when to increment next value
+
+let 
