0f9bd98ab208f1374bf717899ebcbb46a7982adf
samuelhawthorne5 <samuelhawthorne5@penguin.lxd>
2021-08-21 13:31:07 -0500

    new to liliy

---
+++ portfolio2021/script.js
@@ -0,0 +1,16 @@
+
+openNav = () => {
+    let burger = document.getElementById("burgericon")
+   let nav =  document.getElementById("navlist")
+
+  // nav.style.display = "block"
+
+   if(nav.style.display == "none"){
+       nav.style.display = "block"
+
+   }else {
+       nav.style.display = "none"
+   }
+
+
+}
